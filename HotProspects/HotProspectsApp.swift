6ca615459e23071390d725b66a2159a9f2e26e2d
Carlossiii <cv5673@gmail.com>
2022-10-31 12:43:50 -0300

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Carlos Vinicius on 31/10/22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
