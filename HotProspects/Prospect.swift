936afb9cbf7dd138a2de4bb8e9e0d6a1efc99329
Carlossiii <cv5673@gmail.com>
2022-11-03 21:47:51 -0300

    Finalized project.

---
+++ HotProspects/Prospect.swift
@@ -0,0 +1,49 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Carlos Vinicius on 01/11/22.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    fileprivate(set) var isContacted = false
+}
+
+@MainActor class Prospects: ObservableObject {
+    @Published private(set) var people: [Prospect]
+    let saveKey = "SavedData"
+    
+    init() {
+        if let data = UserDefaults.standard.data(forKey: saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                people = decoded
+                return
+            }
+        }
+        
+        // no saved data, meaning that an empty array will be loaded
+        people = []
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: saveKey)
+        }
+    }
+    
+    func add (_ prospect: Prospect) {
+        people.append(prospect)
+        save()
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+        save()
+    }
+}
